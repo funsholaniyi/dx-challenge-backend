c32ec878f3067d8ced7f9858124c48d6fc616ae4
Funsho Olaniyi <funsholaniyi@gmail.com>
2019-06-03 15:58:09 +0000

    Fixed CORS policy issue

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Resource, Api
 
 from models.data import ProductionModel
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(app)
 model = ProductionModel()
 
